b7652756bdb2ab2f1d31fd18b7c181414e7e1a38
Hugo AT <hugo.buenrostroa@agilethought.com>
2022-03-31 11:42:07 -0600

    feat: String Interpolation

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Hugo';
+  age = 30;
+  img = 'https://source.unsplash.com/random';
 }
