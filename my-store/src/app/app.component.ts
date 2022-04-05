4ea32c970c3fd2c45638f682e1e0f56f4382e643
Hugo AT <hugo.buenrostroa@agilethought.com>
2022-04-05 08:28:16 -0500

    feat: Event Binding

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -9,4 +9,11 @@ export class AppComponent {
   name = 'Hugo';
   age = 30;
   img = 'https://source.unsplash.com/random';
+  btnDisabled = false;
+
+  person = {
+    name: 'Hugo',
+    age: 30,
+    avatar: 'https://source.unsplash.com/random'
+  };
 }
