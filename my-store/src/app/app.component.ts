9c97afa253c8b54bd961285731bb753623fd4e18
Hugo AT <hugo.buenrostroa@agilethought.com>
2022-04-05 08:53:08 -0500

    feat: events Binding

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -9,11 +9,31 @@ export class AppComponent {
   name = 'Hugo';
   age = 30;
   img = 'https://source.unsplash.com/random';
-  btnDisabled = false;
+  btnDisabled = true;
 
   person = {
     name: 'Hugo',
     age: 30,
     avatar: 'https://source.unsplash.com/random'
   };
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const elem = event.target as HTMLElement;
+
+    console.log(elem.scrollTop);
+  }
+
+  captureData(event: Event) {
+    const element = event.target as HTMLInputElement;
+
+    this.person.name = element.value;
+  }
 }
